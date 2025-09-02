4fbffeb3062c7bf511c4cdac54ce8bc890675d8a
udkbshermen <shyrmin.zoobooksystems@gmail.com>
2025-09-02 04:23:43 +0000

    task 3

--- Task3.java
+++ Task3.java
@@ -2,12 +2,12 @@ public class Task3{
     public static void main(String[] args) {
         String a = "Wow";
         String b = "Wow";
-        String c = a;
-        String d = c + "!";
+        String c = a + "!";
+        String d = c;
 
         boolean b1 = a == b;
         boolean b2 = d.equals(b + "!");
-        boolean b3 = !c.equals(a + "!");
+        boolean b3 = !c.equals(a);
 
         if (b1 && b2 && b3) {
             System.out.println("Success!");
