d4a726665e6faa6800e149ee142d09218b6aad21
udkbshermen <shyrmin.zoobooksystems@gmail.com>
2025-09-02 04:13:14 +0000

    task 2

--- Task2.java
+++ Task2.java
@@ -1,1 +1,12 @@
+public class Task2{
+    public static void main(String[] args) {
+        boolean notFalse = true;
+        byte l = 1;
+        short o = 0;
+        int e = 3;
+        float two = 2.0f;
+        char d = 'd';
 
+        System.out.println("H"+ e + l + l + o + " w" + o + "r" + l + d + " " + two + " " + notFalse);
+    }
+}
