0597b67c1d601dfdb6645a4872473d4102775353
udkbshermen <shyrmin.zoobooksystems@gmail.com>
2025-09-02 04:10:14 +0000

    task 3

--- Task3.java
+++ Task3.java
@@ -1,1 +1,16 @@
+public class Task3{
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = "Wow";
+        String c = a;
+        String d = c + "!";
 
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a + "!");
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
