aa5f5d06ea8f87aead3ad758dfa7491c05b20b54
udkbshermen <shyrmin.zoobooksystems@gmail.com>
2025-09-12 07:45:58 +0000

    task 7

--- Task7.java
+++ Task7.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Task7 {
 
+    //operator methods
     int Addition(int x, int y){
         return x+y;
     }
@@ -14,35 +15,61 @@ public class Task7 {
         return x*y;
     }
 
-    int Divide(int x, int y){
+    float Divide(float x, float y){
         return x/y;
     }
     public static void main(String[] args) {
+        int num1, num2; String operator;
+
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        int num1 = input.nextInt();
+        //first number input validation loop (for non-int input)
+        while(true){
+            System.out.print("Enter first number: ");
+            if (input.hasNextInt()){
+                num1 = input.nextInt();
+                break;
+            } else{
+                System.out.print("Invalid input. Please try again. ");
+                input.next();
+            }
+        }
 
-        System.out.print("Enter second number: ");
-        int num2 = input.nextInt();
-
-        System.out.print("Enter operator (+, -, *, /) ");
-        String operator = input.next();
+        //second number input validation loop (for non-int input)
+        while(true){
+            System.out.print("Enter second number: ");
+            if (input.hasNextInt()){
+                num2 = input.nextInt();
+                break;
+            } else{
+                System.out.print("Invalid input. Please try again. ");
+                input.next();
+            }
+        }
 
         Task7 t7 = new Task7();
 
-        if (operator.equals("+"))
-            System.out.println(num1 + " + " + num2 + " = " + t7.Addition(num1,num2));
+        //operator input validation loop for non-string and call operation methods to calculate
+        while (true) {
+            System.out.print("Enter operator (+, -, *, /): ");
+            operator = input.next();
 
-        else if (operator.equals("-"))
-            System.out.println(num1 + " - " + num2 + " = " + t7.Subtract(num1,num2));
-        
-        else if (operator.equals("*"))
-            System.out.println(num1 + " x " + num2 + " = " + t7.Multiply(num1,num2));
+            if (operator.equals("+")) {
+                System.out.println(num1 + " + " + num2 + " = " + t7.Addition(num1,num2));
+                break;
+            } else if (operator.equals("-")) {
+                System.out.println(num1 + " - " + num2 + " = " + t7.Subtract(num1,num2));
+                break;
+            } else if (operator.equals("*")) {
+                System.out.println(num1 + " x " + num2 + " = " + t7.Multiply(num1,num2));
+                break;
+            } else if (operator.equals("/")) {
+                System.out.println(num1 + " / " + num2 + " = " + t7.Divide((float)num1,(float)num2));
+                break;
+            } else {
+                System.out.print("Invalid operator. Please try again. ");
+            }
+        }
 
-        else if (operator.equals("/"))
-            System.out.println(num1 + " / " + num2 + " = " + t7.Divide(num1,num2));
-
-        else System.out.println("Invalid Operator. Please try again.");
-    }
+}
 }
--- get.java
+++
@@ -1,9 +0,0 @@
-
-public class get {
-
-    public static char[] Addition(int num1, int num2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Addition'");
-    }
-
-}
